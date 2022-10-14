8fdaf9a0321cc7bd338b66719c8aed66c3ac8de1
Rubén Palmer <40397192+Zygmut@users.noreply.github.com>
2022-10-14 14:51:31 +0200

    Get perceptions from Enviroment

--- src/main/java/agent/Robot.java
+++ src/main/java/agent/Robot.java
@@ -7,7 +7,7 @@ import productionrules.Condition;
 import productionrules.Rule;
 
 public class Robot {
-    
+
     public enum Action {
 
         MOVE_NORTH {
@@ -40,53 +40,64 @@ public class Robot {
             System.out.println("NOT IMPLEMENTED");
         }
     }
-    
-    //==========================================================================
-    
-    
-    private final String[] LABELS = {"p1", "p2", "p3", "p4", "p5", "p6", "p7", "p8"};
+
+    // ==========================================================================
+
+    private final String[] LABELS = { "p1", "p2", "p3", "p4", "p5", "p6", "p7", "p8" };
     private final Characteristic[] characteristics;
-    
+
     private BC bc;
     private Point position;
-    
-    public Robot(){
+
+    public Robot() {
         this.position = new Point();
-        
+
         // Initialize characteristics to add labels
         characteristics = new Characteristic[LABELS.length];
         for (int i = 0; i < characteristics.length; i++) {
             characteristics[i] = new Characteristic(LABELS[i]);
         }
-        
+
         initBC();
     }
-    
-    private void initBC(){
+
+    private void initBC() {
         this.bc = new BC();
-        
-        // ADD RULES
-        
+
+        // TODO: Add rules
     }
-    
-    private void addRule(Rule rule){
+
+    private void addRule(Rule rule) {
         this.bc.addRule(rule);
     }
-    
-    private void addRule(int[] indices, Action action){
+
+    private void addRule(int[] indices, Action action) {
         this.bc.addRule(new Rule(new Condition(selectCharacteristics(this.characteristics, indices)), action));
     }
-    
-    public Action checkBC(){
+
+    public Action checkBC() {
         return this.bc.check();
     }
-    
-    public String printBC(){
+
+    public String printBC() {
         return this.bc.toString();
     }
-    
-    public String printEvaluatedBC(){
+
+    public String printEvaluatedBC() {
         return this.bc.toStringEvaluated();
+    }
+
+    public void processPerceptions(boolean[] perceptions) {
+
+        enum PerceptionIndex {
+            TOP_LEFT, TOP, TOP_RIGHT, LEFT, RIGHT, BOTTOM_LEFT, BOTTOM, BOTTOM_RIGHT
+        }
+
+        // ? DEBUG
+        // System.out.println("Robot perceptions: ");
+        // for (int i = 0; i < PerceptionIndex.values().length; i++) {
+        // System.out.println(PerceptionIndex.values()[i] + ": " + perceptions[i]);
+        // }
     }
 
     public Point getPosition() {
@@ -96,22 +107,19 @@ public class Robot {
     public void setPosition(Point position) {
         this.position = position;
     }
-    
+
     public void setPosition(int x, int y) {
         this.position = new Point(x, y);
     }
-    
-    
-    
-    //==========================================================================
-        
-    private Characteristic[] selectCharacteristics(Characteristic[] characteristics, int[] indices){
+
+    // ==========================================================================
+
+    private Characteristic[] selectCharacteristics(Characteristic[] characteristics, int[] indices) {
         Characteristic[] resultCharacteristics = new Characteristic[indices.length];
         for (int i = 0; i < indices.length; i++) {
             resultCharacteristics[i] = characteristics[indices[i]];
         }
         return resultCharacteristics;
     }
-    
-    
+
 }
--- src/main/java/environment/Environment.java
+++ src/main/java/environment/Environment.java
@@ -4,55 +4,69 @@ package environment;
 import agent.Robot;
 import java.awt.Point;
 
-
 public class Environment {
-    
+
     private Boolean[][] map;
     private Robot robot;
-    
-    public Environment(int n){
+
+    public Environment(int n) {
         this.map = new Boolean[n][n];
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[0].length; j++) {
+                map[i][j] = false;
+            }
+        }
     }
-    
-    public Environment(Boolean[][] map){
+
+    public Environment(Boolean[][] map) {
         this.map = map;
     }
-    
-    public Boolean isObstacle(int i, int j){
+
+    public Boolean isObstacle(int i, int j) {
         return this.map[i][j];
     }
-    
-    public void setObstacle(int i, int j, boolean value){
+
+    public void setObstacle(int i, int j, boolean value) {
         this.map[i][j] = value;
     }
-    
-    public void toggleObstacle(int i, int j){
+
+    public void toggleObstacle(int i, int j) {
         this.map[i][j] = !this.map[i][j];
     }
-    
-    public void runIteration(){
-        boolean[] perceptions = new boolean[8];
-        //perceptions.from(robot, map);
-        //robot.processPerceptions(perceptions); //update characteristics
-        //action = robot.check()
-        //action.execute()
-        //wait animation
+
+    public void setRobot(Robot robot) {
+        this.robot = robot;
     }
-    
-    private boolean[] getPerceptions(){
+
+    public void runIteration() {
+        robot.processPerceptions(getPerceptions(robot));
+        // robot.checkBC().execute();
+
+        // TODO: wait animation
+    }
+
+    private boolean[] getPerceptions(Robot robot) {
         boolean[] perceptions = new boolean[8];
-        
-        Point robot = this.robot.getPosition();
-        for (int i = -1; i < robot.x +1; i++) {
-            for (int j = 0; j < 10; j++) {
-                
-                
+        int idx = 0;
+        Point robotPos = robot.getPosition();
+
+        for (int y = robotPos.y - 1; y <= robotPos.y + 1; y++) {
+            for (int x = robotPos.x - 1; x <= robotPos.x + 1; x++) {
+                if (x == robotPos.x && y == robotPos.y) {
+                    continue;
+                }
+
+                try {
+                    perceptions[idx] = map[y][x];
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    // Map outer perimeter
+                    perceptions[idx] = true;
+                }
+                idx++;
             }
-            boolean perception = perceptions[i];
-            
         }
-        
+
         return perceptions;
     }
-    
+
 }
