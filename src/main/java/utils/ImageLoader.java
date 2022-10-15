80b0ece37d187ecb74a2a60b99dd6557e4d057f8
Cristian Comellas <ccf200@hotmail.com>
2022-10-15 10:14:02 +0200

    Create ImageLoader.java

---
+++ src/main/java/utils/ImageLoader.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package utils;
+
+import gui.Tile;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+
+/**
+ *
+ * @author ccf20
+ */
+public class ImageLoader {
+    
+    public static BufferedImage loadImage(String path){
+        BufferedImage bi = null;
+        try {
+            bi = ImageIO.read(new File(path));
+        } catch (IOException ex) {
+            Logger.getLogger(Tile.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return bi;
+    }
+    
+}
